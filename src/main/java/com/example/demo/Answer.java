892cf34175561cf4c3b12c6a54daa356226d434c
sixdayz6 <win9809@gmail.com>
2025-09-02 11:32:08 +0900

    v2.04 엔티티로 테이블 매핑하기

---
+++ src/main/java/com/example/demo/Answer.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
