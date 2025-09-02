72c129c221aec81ef0f3cd59099b86bbfa92879a
sixdayz6 <win9809@gmail.com>
2025-09-02 09:57:26 +0900

    first commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        System.out.println("index");
+        return "index";
+    }
+}
