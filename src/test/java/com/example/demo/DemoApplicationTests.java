b52eb6d2b0953839631c2762657b4b3d13de2074
sixdayz6 <win9809@gmail.com>
2025-09-03 22:19:56 +0900

    answerFindByQuestionTest 작성 완료

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -113,6 +114,18 @@ class DemoApplicationTests {
             assertEquals(2, a.getQuestion().getId());
         }
 
+        @Transactional
+        @Test
+        void answerFindByQuestionTest() {
+            Optional<Question> oq = this.questionRepository.findById(2);
+            assertTrue(oq.isPresent());
+            Question q = oq.get();
+
+            List<Answer> answerList = q.getAnswerList();
+
+            assertEquals(1, answerList.size());
+            assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+        }
 
     }
 }
