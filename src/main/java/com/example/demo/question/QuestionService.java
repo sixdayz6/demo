44560d17cc9cd1b3dfd2d975c3e6b864f593948c
sixdayz6 <win9809@gmail.com>
2025-09-04 12:08:54 +0900

    v2.10 상세페이지 만들기

--- src/main/java/com/example/demo/question/QuestionService.java
+++ src/main/java/com/example/demo/question/QuestionService.java
@@ -2,8 +2,11 @@ package com.example.demo.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.example.demo.DataNotFoundException;
+
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        }  else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
